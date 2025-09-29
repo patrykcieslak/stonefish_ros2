f85b450b74d030ae190406aeb5713ac8a05d46b1
Anders Høgden <andeshog@stud.ntnu.no>
2025-09-29 18:30:45 +0200

    add helper functions to build-package.sh

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -2,6 +2,14 @@
 set -e
 ROS_WORKSPACE="${WORKSPACE:-$HOME/ros2_ws}"
 
+log_info() {
+    echo -e "$LOG_PREFIX [INFO] $1"
+}
+
+log_error() {
+    echo -e "$LOG_PREFIX [ERROR] $1" >&2
+}
+
 # ----------------------------- BUILD ROS 2 PACKAGES -----------------------------
 build_ros_workspace() {
     log_info "Setting up ROS 2 workspace..."
